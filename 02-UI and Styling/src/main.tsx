ef43a472b6b194eeab9e7ccb793018c62f4b42fd
MOJOEA <DevineTYpor@gmail.com>
2026-05-15 22:01:14 +0700

    Update main.tsx

--- 02-UI and Styling/src/main.tsx
+++ 02-UI and Styling/src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.tsx'
+import Login from './page/Login.tsx'
+import Practice2 from './page/Practice2.tsx'
+
+import Header from './components/header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Practice2 />
   </StrictMode>,
 )
